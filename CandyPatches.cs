ecbaf6f8db67bfbe3fb61ad4f8bf697a0c79e252
Nameless <85962933+Misfiy@users.noreply.github.com>
2023-08-18 23:17:14 +0200

    Optimization

---
+++ CandyPatches.cs
@@ -0,0 +1,26 @@
+// using HarmonyLib;
+// using InventorySystem.Items.Usables.Scp330;
+
+// namespace CandyUtilities.Patches
+// {
+//     [HarmonyPatch(typeof(CandyPink), nameof(CandyPink.SpawnChanceWeight))]
+//     public static class CandyPinkPatch
+//     {
+//         public static bool Prefix(ref float __result)
+//         {
+//             __result = CandyUtil.Instance.Config.PinkChance;
+//             return true;
+//         }
+//     }
+    // [HarmonyPatch(typeof(CandyBlue), nameof(CandyBlue.SpawnChanceWeight))]
+    // public static class CandyBluePatch
+    // {
+    //     public static bool Prefix(ref float __result)
+    //     {
+    //         __result = CandyUtil.Instance.Config;
+    //         return true;
+    //     }
+    // }
+// }
+
+// this will probably never exist kekw
--- CandyUtilities.cs
+++ CandyUtilities.cs
@@ -1,47 +1,44 @@
-﻿namespace CandyUtilities
+﻿namespace CandyUtilities;
+
+using Exiled.API.Features;
+using Exiled.API.Enums;
+using CandyUtilities.Events;
+using Scp330 = Exiled.Events.Handlers.Scp330;
+
+public class CandyUtil : Plugin<Config>
 {
-     using Exiled.API.Features;
-     using Exiled.API.Enums;
-     using CandyUtilities.Events;
-     using Scp330 = Exiled.Events.Handlers.Scp330;
-
-     public class CandyUtil : Plugin<Config>
+     public override string Name => "Candy Utilities";
+     public override string Prefix => "CandyUtils";
+     public override string Author => "@misfiy";
+     public override PluginPriority Priority => PluginPriority.Default;
+     private EventHandler eventHandler;
+     public static CandyUtil Instance;
+     private Config config;
+     public override void OnEnabled()
      {
-          public override string Name => "Candy Utilities";
-          public override string Prefix => "CandyUtils";
-          public override string Author => "@misfiy";
-          public override PluginPriority Priority => PluginPriority.Default;
-          private EventHandler eventHandler;
-          public static CandyUtil Instance;
-          private Config config;
-          public override void OnEnabled()
-          {
-               Instance = this;
-               config = Instance.Config;
+          Instance = this;
+          config = Instance.Config;
 
-               RegisterEvents();
-               base.OnEnabled();
-          }
+          RegisterEvents();
+          base.OnEnabled();
+     }
 
-          public override void OnDisabled()
-          {
-               UnregisterEvents();
-               Instance = null;
-               base.OnDisabled();
-          }
-          public void RegisterEvents()
-          {
-               eventHandler = new EventHandler();
-               Scp330.InteractingScp330 += eventHandler.OnInteraction;
+     public override void OnDisabled()
+     {
+          UnregisterEvents();
+          Instance = null;
+          base.OnDisabled();
+     }
+     public void RegisterEvents()
+     {
+          eventHandler = new EventHandler();
+          Scp330.InteractingScp330 += eventHandler.OnInteraction;
 
-               Log.Debug("Events have been registered!");
-          }
-          public void UnregisterEvents()
-          {
-               Scp330.InteractingScp330 -= eventHandler.OnInteraction;
-               eventHandler = null;
-          }
+          Log.Debug("Events have been registered!");
+     }
+     public void UnregisterEvents()
+     {
+          Scp330.InteractingScp330 -= eventHandler.OnInteraction;
+          eventHandler = null;
      }
 }
-
-// player.TryAddCandy(InventorySystem.Items.Usables.Scp330.CandyKindID.Pink);
